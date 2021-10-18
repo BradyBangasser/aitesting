13f0928dc4bad0a9d8a45610f79a4d58d9898ed7
bradyinnovation <bradyincorporated2@gmail.com>
2021-10-18 18:38:16 -0500

    Hmmmmmmmmmmmmmmmmmm

--- index.js
+++ index.js
@@ -1,59 +1,81 @@
-var net
-const urls = ['./images/Bradyjay.jpg', 'https://i.imgur.com/HBrB8p0', 'https://i.imgur.com/UXN9GD8.jpeg', 'https://i.imgur.com/e1FM4b8.jpeg', 'https://i.imgur.com/wuPCAty.jpeg', 'https://i.imgur.com/XCBF317.jpeg', 'https://i.imgur.com/J7qY2ea.jpeg', 'https://i.imgur.com/Kv1faYM.jpeg']
-const classifier = knnClassifier.create();
-function save(model) {
-  model.save()
-}
-function changeImg() {
-  const url = urls[Math.floor(Math.random() * urls.length)]
-  document.getElementById("img").src=url;
-  return url
-}
 async function app() {
-    console.log('Loading mobilenet..');
-  
-    // Load the model.
-    net = await mobilenet.load();
-    console.log('Successfully loaded model');
-  
-    // Create an object from Tensorflow.js data API which could capture image 
-    // from the web camera as Tensor.
-  
-    // Reads an image from the webcam and associates it with a specific class
-    // index.
-    const addExample = async classId => {
-      // Capture an image from the web camera.
-      const img = document.getElementById('img')
-      if (classifier.getNumClasses() > 0) {
-        changeImg()
-        const img = document.getElementById('img');
-  
-        // Get the activation from mobilenet from the webcam.
-        const activation = net.infer(img, 'conv_preds');
-        // Get the most likely class and confidence from the classifier module.
-        const result = await classifier.predictClass(activation);
-  
-        const classes = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 's11'];
-        document.getElementById('console').innerText = `
-          prediction: ${classes[result.label]}\n
-          probability: ${result.confidences[result.label]}
-        `;
-  
-        // Dispose the tensor to release the memory.
-        //img.dispose();
-      }
-  
-      // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-      // to the KNN classifier.
-      const activation = net.infer(img, true);
-  
-      // Pass the intermediate activation to the classifier.
-      classifier.addExample(activation, classId);
-  
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Create an object from Tensorflow.js data API which could capture image 
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = async classId => {
+    // Capture an image from the web camera.
+    const img = await webcam.capture();
+
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(img, true);
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+
+    // Dispose the tensor to release the memory.
+    img.dispose();
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('m1').addEventListener('click', () => addExample(0));
+  document.getElementById('m2').addEventListener('click', () => addExample(1));
+  document.getElementById('m3').addEventListener('click', () => addExample(2));
+  document.getElementById('m4').addEventListener('click', () => addExample(3));
+  document.getElementById('m5').addEventListener('click', () => addExample(4));
+  document.getElementById('m6').addEventListener('click', () => addExample(5));
+  document.getElementById('m7').addEventListener('click', () => addExample(6));
+  document.getElementById('m8').addEventListener('click', () => addExample(7));
+  document.getElementById('m9').addEventListener('click', () => addExample(8));
+  document.getElementById('m10').addEventListener('click', () => addExample(9));
+  document.getElementById('f1').addEventListener('click', () => addExample(10));
+  document.getElementById('f2').addEventListener('click', () => addExample(11));
+  document.getElementById('f3').addEventListener('click', () => addExample(12));
+  document.getElementById('f4').addEventListener('click', () => addExample(13));
+  document.getElementById('f5').addEventListener('click', () => addExample(14));
+  document.getElementById('f6').addEventListener('click', () => addExample(15));
+  document.getElementById('f7').addEventListener('click', () => addExample(16));
+  document.getElementById('f8').addEventListener('click', () => addExample(17));
+  document.getElementById('f9').addEventListener('click', () => addExample(18));
+  document.getElementById('f10').addEventListener('click', () => addExample(19));
+  document.getElementById('dawg').addEventListener('click', () => changeImg());
+  document.getElementById('brady').addEventListener('click', () => addExample(20));
+  document.getElementById('save').addEventListener('click', async () => {await net.save()});
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture();
+
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(img, 'conv_preds');
+      // Get the most likely class and confidence from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 's11'];;
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.label]}\n
+        probability: ${result.confidences[result.label]}
+      `;
+
       // Dispose the tensor to release the memory.
-      //img.dispose();
-    };
-    // When clicking a button, add an example for that class.
+      img.dispose();
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+/*
+const classes = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 's11'];
     document.getElementById('m1').addEventListener('click', () => addExample(0));
     document.getElementById('m2').addEventListener('click', () => addExample(1));
     document.getElementById('m3').addEventListener('click', () => addExample(2));
@@ -77,5 +99,4 @@ async function app() {
     document.getElementById('dawg').addEventListener('click', () => changeImg());
     document.getElementById('brady').addEventListener('click', () => addExample(20));
     document.getElementById('save').addEventListener('click', async () => {await net.save()});
-  }
-app();
+*/
