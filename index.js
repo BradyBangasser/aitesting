f07add51318d907446c490c32aa67456e9b748f9
bradyinnovation <bradyincorporated2@gmail.com>
2021-10-18 17:04:04 -0500

    yes

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function app() {
         // Get the most likely class and confidence from the classifier module.
         const result = await classifier.predictClass(activation);
   
-        const classes = ['A', 'B', 'C'];
+        const classes = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10'];
         document.getElementById('console').innerText = `
           prediction: ${classes[result.label]}\n
           probability: ${result.confidences[result.label]}
